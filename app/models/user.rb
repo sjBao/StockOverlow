0e48fef37ad94fe26e965c531a4fdda58dfa8ac1
John Talio & Nick Himmel & Tony Lam <pair+johntalio.nickhimmel.sjbao@devbootcamp.com>
2017-01-30 12:26:14 -0500

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  has_secure_password
+
+  has_one :portfolio
+  has_many :positions, through: :portfolio
 end
