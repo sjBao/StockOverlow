0ef89367e20df9ee4c3e35751ad1fb5ccd13e3c3
John Talio <johntalio@yahoo.com>
2017-01-30 17:08:54 -0500

    Shifted routes down to avoid merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
 
 
+
+
+  root 'positions#index'
   get '/portfolios/new' => 'portfolios#new'
   post '/portfolios' => 'portfolios#create'
   get 'portfolios/:id/positions/:id' => 'positions#show'
