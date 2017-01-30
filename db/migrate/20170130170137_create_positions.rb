ed6bd81bc6bcf963db16cc6bf18d730c06a40f88
John Talio & Nick Himmel & Tony Lam <pair+johntalio.nickhimmel.sjbao@devbootcamp.com>
2017-01-30 12:07:36 -0500

    Write migrations

--- db/migrate/20170130170137_create_positions.rb
+++ db/migrate/20170130170137_create_positions.rb
@@ -1,6 +1,10 @@
 class CreatePositions < ActiveRecord::Migration[5.0]
   def change
     create_table :positions do |t|
+      t.references :portfolio
+      t.integer :shares, :null => false
+      t.integer :buy_price, :null => false
+      t.string :ticker, :null => false
 
       t.timestamps
     end
