2ca99bf904906375acbb3a1ff5cf743c060ee7a9
Nick Himmel <email": null>
2017-01-30 12:01:55 -0500

    Add models and migrations for User, Portfolio and Position

---
+++ spec/models/position_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+Rspec.describe Position, :type => :model do
+  describe 'it belongs to a user' do
+    it '#user returns user who bought this stock'
+  end
+
+  describe '#current_value' do
+    it 'returns the current value of the position'
+  end
+
+  describe '#cost_basis'do
+    it 'returns the buy price times the number of shares'
+  end
+
+  describe 'buy price' do
+    it 'has a buy price'
+  end
+
+  describe 'shares' do
+    it 'returns the amount of shares purchased'
+  end
+
+  describe '#profit_loss' do
+    it 'calculates profit loss'
+  end
+
+  describe 'ticker' do
+    it 'returns ticker'
+  end
+end
