0d17e73bbd3f5b8db89ad01c1dd31fd9dfa3e652
John Talio <johntalio@yahoo.com>
2017-01-30 17:28:39 -0500

    Portfolio Index
    * Create some routes for portfolios and positions
    
    * Add some controller routes for portfolios and positions
    
    * Create view folders for portfolios and positions
    
    * Shifted routes down to avoid merge conflicts
    
    * Add possible error message for wrongly creating portfolio
    
    * Add more possibilities to api query
    
    * Display info on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "positions#index"
-  get "/login", to: "sessions#new"
+  root 'positions#index'
+  get  '/portfolios/new' => 'portfolios#new'
+  post '/portfolios' => 'portfolios#create'
+  get  '/portfolios/:id/positions/:id' => 'positions#show'
+
+  get  "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  get  "/logout", to: "sessions#destroy"
 
   resources :users, except: :destroy 
 end
