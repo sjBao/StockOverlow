ebfd5a6097fc59b266de87e645eaedae27330ca4
sjBao <supuilam@gmail.com>
2017-01-30 16:23:03 -0500

    Complete user authentification

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  include SessionsHelper
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      log_in(@user)
+      redirect_to '/'
+    else
+      @errors = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+end
