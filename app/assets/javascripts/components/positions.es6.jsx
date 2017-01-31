e3d66c6be8bade31a5fc36e7d87256704b61de29
John Talio & Nick Himmel & Tony Lam <pair+johntalio.nickhimmel.sjbao@devbootcamp.com>
2017-01-31 10:41:26 -0500

    Fix bugs

--- app/assets/javascripts/components/positions.es6.jsx
+++ app/assets/javascripts/components/positions.es6.jsx
@@ -1,8 +1,8 @@
-class Positions extends React.Component {
-
-  render() {
-    return (
-
-    )
-  }
-}
+// class Positions extends React.Component {
+//
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
