6388d7a9d5bbb0c32c530979088a84d2589a2c1c
= <=>
2021-01-09 15:29:28 -0500

    Upgrade to rails 6.1

--- db/migrate/20170130170124_create_portfolios.rb
+++ db/migrate/20170130170124_create_portfolios.rb
@@ -1,8 +1,9 @@
-class CreatePortfolios < ActiveRecord::Migration[5.0]
+class CreatePortfolios < ActiveRecord::Migration[6.1]
   def change
     create_table :portfolios do |t|
       t.references :user
-       
+      t.string :name, null: false
+
       t.timestamps
     end
   end
