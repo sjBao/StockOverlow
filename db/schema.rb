181fc1ddf5cfdca4a0bcfc79f11dc1b68ed8e0d1
= <=>
2021-01-09 16:20:40 -0500

    Enable UUID to be used as primary_key

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,10 @@
 ActiveRecord::Schema.define(version: 2017_01_30_170137) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "portfolios", force: :cascade do |t|
+  create_table "portfolios", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.bigint "user_id"
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -35,7 +36,7 @@ ActiveRecord::Schema.define(version: 2017_01_30_170137) do
     t.index ["user_id"], name: "index_positions_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "email", null: false
